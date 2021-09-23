6af676077a861195a3b83fc53bd14c3990b05ce8
HirosueAkari <hirosue_akari@retail-ai.jp>
2021-09-23 10:13:23 +0900

    入力値を取得する

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ interface Store {
 
 function App() {
   const [data, setData] = useState<Store[]>([])
+  const [storeName, setStoreName] = useState('')
   const search = () => {
     axios.get('https://jsonplaceholder.typicode.com/pasts').then((res: AxiosResponse) => {
       setData(res.data)
@@ -18,8 +19,19 @@ function App() {
       setData(storeList)
     })
   }
+
+  const onChangeValue = (el: React.ChangeEvent<HTMLInputElement>) => {
+    setStoreName(el.target.value)
+  }
   return (
     <div className="App">
+      <div className="form">
+        <input
+          type="text"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChangeValue(e)}
+        />
+        <p>{storeName}</p>
+      </div>
       <button className="searchBtn" onClick={search}>検索</button>
       <ul>{data.map((store: Store) => <li key={store.code}> {store.name} </li>)}</ul>
     </div>
