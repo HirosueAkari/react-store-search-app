a1faf54b2404728e65aa6004c26aed02f329ff35
HirosueAkari <hirosue_akari@retail-ai.jp>
2021-10-29 18:26:41 +0900

    緯度経度から住所を取得する

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,11 +1,13 @@
 import App from 'pages/App'
 import { useState } from 'react'
 import { Button } from '@material-ui/core'
+import Geocode from "react-geocode"
 import { makeStyles, createStyles } from "@material-ui/core/styles"
 
 export default function Conditions(): JSX.Element {
   const [latitude, setLatitude] = useState<number>()
   const [longitude, setLongitude] = useState<number>()
+  const [localLocation, setLocalLocation] = useState<string>('')
 
   const styles = makeStyles(() =>
     createStyles({
@@ -33,6 +35,16 @@ export default function Conditions(): JSX.Element {
   const success = (location: GeolocationPosition) => {
     setLatitude(location.coords.latitude)
     setLongitude(location.coords.longitude)
+    const lat: number = location.coords.latitude
+    const lon: number = location.coords.longitude
+
+    Geocode.setApiKey('AIzaSyA-7S1CFzW0z10dZ72KrpvbX2qdZxktgY4')
+    Geocode.setLanguage('ja')
+    Geocode.setRegion('ja')
+
+    Geocode.fromLatLng(String(lat), String(lon)).then((res) => {
+      setLocalLocation(res.result[0].formatted_address)
+    })
   }
 
   return (
@@ -44,6 +56,7 @@ export default function Conditions(): JSX.Element {
         <div>
           <p>緯度：{latitude}</p>
           <p>経度：{longitude}</p>
+          <p>現在地：{localLocation}</p>
         </div>
       </div>
     </App>
